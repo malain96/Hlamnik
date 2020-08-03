f721593e95c7c151946b3ca52acb4ce1e2fa108c
Mathieu Alain <alain.mathieu1996@gmail.com>
2020-08-03 20:08:51 +0200

    Can now crop an image

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:hlamnik/themes/main_theme.dart';
 import 'package:hlamnik/widgets/error_text.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 
 ///Widget used to display an image picker input
@@ -45,20 +46,29 @@ class _ImageInputState extends State<ImageInput> {
     await _handlePickedFile(imageFile);
   }
 
-  ///Turns an [PickedFile] into base64 string and calls the passed onSelectImage
-  Future<void> _handlePickedFile(PickedFile imageFile) async{
+  ///Crops and turns an [PickedFile] into base64 string and calls the passed onSelectImage
+  Future<void> _handlePickedFile(PickedFile imageFile) async {
     if (imageFile == null) {
       return;
     }
+
+    final croppedImage = await ImageCropper.cropImage(
+      sourcePath: imageFile.path,
+      maxWidth: 1080,
+      maxHeight: 1080,
+    );
+
     setState(() {
-      _image = File(imageFile.path);
+      _image = File(croppedImage.path);
     });
-    final imageBytes = await imageFile.readAsBytes();
+    final imageBytes = await croppedImage.readAsBytes();
     widget.onSelectImage(base64Encode(imageBytes));
   }
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: Column(
@@ -67,35 +77,39 @@ class _ImageInputState extends State<ImageInput> {
           Row(
             children: <Widget>[
               Container(
-                width: 150,
-                height: 100,
+                width: size.width/2,
+                height: size.width/2,
                 decoration: BoxDecoration(
                   border: Border.all(
                     color: widget.error != null
-                        ? Theme.of(context).errorColor
+                        ? Theme
+                        .of(context)
+                        .errorColor
                         : AppColors.secondaryColor,
                   ),
                   borderRadius: BorderRadius.circular(30),
                 ),
                 child: widget.defaultImage != null || _image != null
                     ? ClipRRect(
-                        borderRadius: BorderRadius.circular(30),
-                        child: _image != null
-                            ? Image.file(
-                                _image,
-                                fit: BoxFit.cover,
-                                width: double.infinity,
-                              )
-                            : widget.defaultImage,
-                      )
+                  borderRadius: BorderRadius.circular(30),
+                  child: _image != null
+                      ? Image.file(
+                    _image,
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                  )
+                      : widget.defaultImage,
+                )
                     : Text(
-                        'noImage'.tr(),
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: widget.error != null
-                                ? Theme.of(context).errorColor
-                                : AppColors.secondaryColor),
-                      ),
+                  'noImage'.tr(),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: widget.error != null
+                          ? Theme
+                          .of(context)
+                          .errorColor
+                          : AppColors.secondaryColor),
+                ),
                 alignment: Alignment.center,
               ),
               SizedBox(
